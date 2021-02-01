525f337aa68a4b0debe8ceb6befc106d8565b052
tarrantwalter <twalter@cub.uca.edu>
2021-02-01 13:53:57 -0600

    finishing lab

--- main.cpp
+++ main.cpp
@@ -24,8 +24,19 @@ int main()
 	ll->display();
 	ll->erase(3);
 	ll->display();
-	// #3, Add 5 inserts
-	// #6, Add 5 erases
+	ll->insert(20, 2);
+	ll->insert(70, 5);
+	ll->insert(100, 0);
+	ll->insert(10, 5);
+	ll->insert(110, 6);
+	ll->display();
+	ll->erase(3);
+	ll->erase(5);
+	ll->erase(4);
+	ll->erase(2);
+	ll->erase(1);
+	ll->display();
+
 	delete ll;
 
 	cin.get();
